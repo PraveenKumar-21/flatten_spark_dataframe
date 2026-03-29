973c15ac07da0987d27c762ae37bcd1ab45f9b78
Praveen Kumar B <bpraveenkumar21@gmail.com>
2026-03-29 11:48:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="flatten_spark_dataframe",
-    version="0.0.1",
+    version="0.0.2",
     author="Praveen Kumar B",
     author_email="bpraveenkumar21@gmail.com",
     description="Databricks PySpark module to flatten nested spark dataframes, basically struct and array of struct till the specified level",
-    keywords = ['PySpark flatten dataframe', 'Databricks PySpark flatten dataframe', 'PySpark nested dataframe', 'databricks pyspark nested dataframe', 'flatten dataframe', 'nested dataframe'],
+    keywords = ['PySpark flatten dataframe', 'Databricks PySpark flatten dataframe', 'Databricks PySpark flatten dataframe level wise', 'PySpark nested dataframe', 'databricks pyspark nested dataframe', 'flatten dataframe', 'nested dataframe'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PraveenKumar-21/flatten_spark_dataframe",
